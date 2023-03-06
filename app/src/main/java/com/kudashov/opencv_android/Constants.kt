aa8b71ffc4cbf312330e5294c71da3bdb88d79f8
kudashov <kudashov@surf.dev>
2023-03-06 19:30:35 +0300

    Верстка тестового экрана

---
+++ app/src/main/java/com/kudashov/opencv_android/Constants.kt
@@ -0,0 +1,3 @@
+package com.kudashov.opencv_android
+
+const val IMAGE_TYPE = "image/*"
--- app/src/main/java/com/kudashov/opencv_android/MainActivity.kt
+++ app/src/main/java/com/kudashov/opencv_android/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.kudashov.opencv_android
 
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.kudashov.opencv_android.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.Main + SupervisorJob()
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var imageBitmap: Bitmap? = null
+
+    private var resultLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let {
+            imageBitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+
+            try {
+                val mat = Mat()
+                Utils.bitmapToMat(imageBitmap, mat)
+                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY)
+                Utils.matToBitmap(mat, imageBitmap)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            Glide.with(this)
+                .load(imageBitmap)
+                .into(binding.pictureIv)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListeners()
+        OpenCVLoader.initDebug()
+    }
+
+    private fun initListeners() = with(binding) {
+        openGalleryBtn.setOnClickListener { resultLauncher.launch(IMAGE_TYPE) }
     }
 }
